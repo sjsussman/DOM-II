635e6b88a161debc7792ee50022208c525ad336e
Steven Sussman <sjsussman@gmail.com>
2020-09-01 15:03:14 -0400

    did 7 events

--- js/index.js
+++ js/index.js
@@ -21,3 +21,44 @@ copyright.addEventListener('mouseover', ()=>
     copyright.style.transform = 'scale(1.5)'
     copyright.style.transform = 'transform 1s'
 })
+
+
+//change color to h2
+let h2 = document.querySelectorAll('h2')
+h2.forEach((h2) => {
+    h2.addEventListener('dblclick', () => {
+      h2.style.color = 'blue'
+    });
+  });
+
+
+//change color of the nav links when space is hit
+document.addEventListener('keydown', (event) => {
+    //if the event object contains the key 'escape' kill modal
+    if (event.key === 'Escape'){
+        document.body.style.backgroundColor = 'red';
+    }
+})
+
+
+
+//add script to alert when image is moved
+const imgs = document.querySelectorAll('img')
+imgs.forEach(img => {
+    img.addEventListener('drag', () => alert('Hi friend! Please put me back :)'));
+  });
+
+//change header font size when window is resized
+window.addEventListener('resize', (event) =>{
+     header.style.fontSize = '10em'
+    });
+
+
+//Create an alert when there is a selection change
+document.addEventListener('selectionchange', (event) => {
+    alert('What was wrong with me!?'); 
+    event.stopPropagation();
+    });
+
+
+    //propagation?
